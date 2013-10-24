fda209c5912c9fa5783756776b3e621def8b3045
Yury <verhalan@gmail.com>
2013-10-24 06:47:46 -0700

    Update main.js

--- jsourse/main.js
+++ jsourse/main.js
@@ -1,5 +1,8 @@
 var app = (function(window, document){
   var canvasel = document.createElement('canvas');
+  canvasel.style.position = 'absolute';
+  canvasel.style.top = '0px';
+  canvasel.style.left = '0px';
   canvasel.width = document.documentElement.clientWidth;
   canvasel.height = document.documentElement.clientHeight;
   var ctsel = canvasel.getContext('2d');
