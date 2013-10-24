de104ee61a4b11afd49c276f8a33f5c6e27695fe
Yury <verhalan@gmail.com>
2013-10-24 06:41:18 -0700

    Create main.js

---
+++ jsourse/main.js
@@ -0,0 +1,7 @@
+var app = (function(window){
+  var canvasel = document.createElement('canvas');
+  canvasel.width = document.documentElement.clientWidth;
+  canvasel.height = document.documentElement.clientHeight;
+  var ctsel = canvas.getContext('2d');
+  return {};
+})(window);
