c876a1e55909713a0acb1fac04737a4a4c8672a6
Yury <verhalan@gmail.com>
2013-10-24 06:41:59 -0700

    Update main.js

--- jsourse/main.js
+++ jsourse/main.js
@@ -2,6 +2,6 @@ var app = (function(window){
   var canvasel = document.createElement('canvas');
   canvasel.width = document.documentElement.clientWidth;
   canvasel.height = document.documentElement.clientHeight;
-  var ctsel = canvas.getContext('2d');
+  var ctsel = canvasel.getContext('2d');
   return {};
 })(window);
