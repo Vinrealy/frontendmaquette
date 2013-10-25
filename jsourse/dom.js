9435dd312cb8f0b517051112b5ae03e7241353f5
Yury <verhalan@gmail.com>
2013-10-25 03:26:06 -0700

    Update dom.js

--- jsourse/dom.js
+++ jsourse/dom.js
@@ -2,7 +2,17 @@ var mainstack = [];
 (function(){
   
   app.init();
-  mainstack.unshift(app.kvad(20,20,40,40));
+  //mainstack.unshift(app.kvad(20,20,40,40));
   app.draw(mainstack);
   
 })();
+
+(function(){
+  var mains = [];console.log(mains);
+  var olol = function(){
+    return {
+      lol: function(){console.log('olol: lol');}
+    };
+  }
+  mains.push('olol.lol()'); console.log(mains);
+})()
